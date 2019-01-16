bc7ba15e8faebe874ab4347c7fdb390cf19388bf
Zach Thomas <zach.thomas@stdesco.com>
2019-01-16 18:54:27 +1000

    Worked out how to do simple Java. Yay

--- src/main/java/com/stdesco/swisstab/webapp/AddTeamPost.java
+++ src/main/java/com/stdesco/swisstab/webapp/AddTeamPost.java
@@ -8,14 +8,17 @@ import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.sun.javafx.fxml.PropertyNotFoundException;
 
 /**
  * Copyright (C) Zachary Thomas - All Rights Reserved
@@ -32,51 +35,58 @@ import com.google.appengine.api.datastore.KeyFactory;
  *
  */
 
-@WebServlet("/addTeam")
-public class AddTeamPost {
-	private final static Logger LOGGER = 
-			Logger.getLogger(InitialisationPost.class.getName());
-	List<String> teams = new ArrayList<String>();
+@SuppressWarnings("serial")
+@WebServlet({"Add Team Servlet", "/addTeam"})
+public class AddTeamPost extends HttpServlet {
+	private static final Logger LOGGER = 
+			Logger.getLogger(AddTeamPost.class.getName());
 	
+	
+	@SuppressWarnings("unchecked") // This is checked, but Eclipse can't
+								   // see Obj based checking.
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) 
-			throws ServletException, IOException, EntityNotFoundException {
-		
+			throws ServletException, IOException {
+	
 		// Sets variables
-		// TODO Deal with Exceptions
 		DatastoreService datastore = 
 				DatastoreServiceFactory.getDatastoreService();
 		int tournamentID = Integer.parseInt(req.getParameter("tid"));
 		String teamName = req.getParameter("tname");
+		List <String> teams = new ArrayList<String>();
+		Entity tournament = null;
 		
 		// Retrieve tournament data from database
 		Key tournamentKey = new KeyFactory.Builder("Provider", 789)
 				.addChild("Tournament", tournamentID)
 				.getKey();
-		com.google.appengine.api.datastore.Entity tournament = 
-				datastore.get(tournamentKey);
-		System.out.println(tournament.getProperty("teams"));
-		System.out.println(tournament.getProperty("teams").getClass().getName());
+		
 		try {
-			@SuppressWarnings("unchecked") // This is fine because the datastore
-										   // is hard coded to set teams to 
-										   // List<String>. If not List<String>
-			                               // then it doesn't exist -> Exception
-			List<String> teams = (List<String>) tournament.getProperty("teams");
-			// Store new list
-			teams.add(teamName);
-			tournament.setProperty("teams", teams);
-			datastore.put(tournament);
-			
-			// Print new team list
-			PrintWriter out = resp.getWriter();
-			
-			out.println("<b>NEW TEAMS LIST: </b><br />");
-			out.println(teams);
-			out.println("<a href= \"index.html\">Return Home</a>");
-		} catch(Exception e) {
-			LOGGER.severe("Ehrm...propertyName \"teams\" might not exist?");
-			// TODO Handle this exception
+			tournament = datastore.get(tournamentKey);
+		} catch(EntityNotFoundException e) {
+			LOGGER.severe("No Entity was found with tournamentKey");
+			e.printStackTrace();
+			// TODO Handle this Exception, probably by re-requesting the 
+			// tournamentID. If it's the Provider ID then we got issues.
 		}
-	}
+		
+		//TODO Test this code after successful test
+		if (tournament.getProperty("teams") != null) {
+			// there is at least one team already added to the tournament
+			teams = (List<String>) tournament.getProperty("teams");
+		}
+			
+		// Store new list
+		teams.add(teamName);
+		System.out.println("Added Team Name: " + teams);
+		tournament.setProperty("teams", teams);
+		datastore.put(tournament);
+		
+		// Print new team list
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<b>NEW TEAMS LIST: </b><br />");
+		out.println(teams);
+		out.println("<br /><a href= \"index.html\">Return Home</a>");
+	} 
 
 }
