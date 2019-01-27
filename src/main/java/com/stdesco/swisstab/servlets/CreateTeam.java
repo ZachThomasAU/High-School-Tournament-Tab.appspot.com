b640bacf4c04c4e0dc30ea2d19673c7d1bcf7726
Jlwin <jeremy.lwin@stdesco.com>
2019-01-27 14:13:02 +1000

    Edited Functionality
    Added ability to grab providerID and current tournamentID

--- src/main/java/com/stdesco/swisstab/servlets/CreateTeam.java
+++ src/main/java/com/stdesco/swisstab/servlets/CreateTeam.java
@@ -29,8 +29,11 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 
 /**
- * Servlet implementation class UpdateUsername
- * 
+ * Servlet for running the process to create a new team 
+ * req passes in a teamname and tournament name.
+ * The team is added to the datastore with its variables
+ * initialized and then added to the list of team names 
+ * in the tournament with tournament name 
  */
 
 @WebServlet("/CreateTeam")
@@ -42,9 +45,10 @@ public class CreateTeam extends HttpServlet {
   DatastoreService datastore = 
 		  			DatastoreServiceFactory.getDatastoreService();
   
-  private String teamname;
-  private String tournamentname;
-  private int tournamentcode;
+  private String teamName;
+  private String tournamenTname;
+  private int tournamentID;
+  private int providerID;
   private Entity tournamentEntity;
   private List<String> teams;
  
@@ -54,17 +58,30 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	  
 	  System.out.print("CreateTeam:51: Running \n");
 	  
+	  //initialize the hash-map for response back to web-app
 	  Map<String, Object> map = new HashMap<String, Object>();
 	  int respcode = 0;
-
-	  teamname = req.getParameter("teamname");
-	  tournamentname = req.getParameter("tournamentname");
+	  teamName = req.getParameter("teamname");
+	  tournamenTname = req.getParameter("tournamentname");
 	  
-      /* Use a google data-store query to check whether or not the tournament 
+	  Key globalsKey = KeyFactory.createKey("Globals", "highschool");
+      Entity globals;
+		try {
+			globals = datastore.get(globalsKey);
+			providerID = Math.toIntExact((long) 
+									globals.getProperty("providerID"));
+		} catch (EntityNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	  
+      /* 
+       * Use a google data-store query to check whether or not the tournament 
        * that the team is trying to assign to has already been created
        */
       Filter propertyFilter =
-          new FilterPredicate("tournamentName", FilterOperator.EQUAL, tournamentname);
+          new FilterPredicate("tournamentName", FilterOperator.EQUAL, 
+        		  												tournamenTname);
       
       //initialize the query
       Query q = new Query("Tournament").setFilter(propertyFilter);
@@ -76,6 +93,8 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
         System.out.print("Query result" + qresult.toString() + "\n");
         //yes it exists you are good to go         
         respcode = 0;
+        tournamentID = Math.toIntExact((long) 
+  	    						qresult.getProperty("tournamentID"));
         
         
       } catch (Exception e) {        
@@ -84,32 +103,39 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
         System.out.print("Query result :" + "NULL" + ": \n");
         //no tournament was found with that name exit the servlet 
         respcode = 1;
+        //Respond to client that the tournamentName does not exist
+        map.put("respcode", respcode);       
+        write(resp, map);
+        //Abort Process
         return;
       }
       
+      //TODO: Implement checking for the team already existing
+      
       //Create an entity of kind "Team" in the data-store
       // Create the new provider Entity
-	  Entity team = new Entity("Team", teamname);
+	  Entity team = new Entity("Team", teamName);
 	  team.setProperty("tournamentTeamID", null);
 	  team.setProperty("tournamentScore", 0);
 	  team.setProperty("tournamentByeRound", 0);
       team.setProperty("coach", null);
       team.setProperty("players", null);    
 	  datastore.put(team);     
+
 	  
-	  //Pull the
+	  //Create a key for the Tournament that has been given 
 	  
-	  Key key = new KeyFactory.Builder("Provider", 269)
-			  .addChild("Tournament", 2389)
+	  Key tourKey = new KeyFactory.Builder("Provider", providerID)
+			  .addChild("Tournament", tournamentID)
 			  .getKey();
 	  try {
-		tournamentEntity = datastore.get(key);
+		tournamentEntity = datastore.get(tourKey);
 		try {
 			teams = (List<String>) tournamentEntity.getProperty("teams");
-			teams.add(teamname);
+			teams.add(teamName);
 		} catch (NullPointerException e) {
 			teams = new ArrayList<String>();
-			teams.add(teamname);
+			teams.add(teamName);
 			System.out.print("CreateTeam:112: "
 					+ "Caught Null Pointer Exception \n");
 		}
@@ -122,27 +148,27 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
 		e.printStackTrace();
 	}
 
-	  
-	  System.out.print("CreateTeam:121: Created New Team \n");
-	  
-      
-      //Add the team to the list of teams in the 
-      
+	  //Return to client that the creation of the team has been successful
+	  System.out.print("CreateTeam:135: Created New Team \n");
+	  map.put("respcode", respcode);       
+      write(resp, map);    
+          
  }
 	
 	/**
-	 * I don't know what this does. 
-	 * It writes back to the web-app dummy?
+	 * Writes the HttpServletResponse resp back to the web-application
+	 * Map is sent back a JSON which can be accessed by the AJAX callback
+	 * function and return information to user
 	 * @param resp
 	 * @param map
 	 * @throws IOException
 	 */
-	@SuppressWarnings("unused")
 	private void write(HttpServletResponse resp, Map<String, Object> map)
 			throws IOException {
 		resp.setContentType("application/json");
 		resp.setCharacterEncoding("UTF-8");
-		System.out.print("Sending back response to the webapp -> GSON\n");
+		System.out.print("CreateTeam:154: Sending JSON Response \n");
+		System.out.print(new Gson().toJson(map).toString() + "\n");
 		resp.getWriter().write(new Gson().toJson(map));
 	}
 
