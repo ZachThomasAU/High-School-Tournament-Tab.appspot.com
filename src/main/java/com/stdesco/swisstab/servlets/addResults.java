4a1f155343cd1e045be264108ffe0cbe3204463a
Zach Thomas <zach.thomas@stdesco.com>
2019-01-30 18:46:29 +1000

    Cleaned up unused import
    Specifically removed gson import from addResults.java

--- src/main/java/com/stdesco/swisstab/servlets/addResults.java
+++ src/main/java/com/stdesco/swisstab/servlets/addResults.java
@@ -9,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
 import com.stdesco.swisstab.utils.DatastoreUtils;
 import com.stdesco.swisstab.utils.ServletUtils;
 import com.google.appengine.api.datastore.DatastoreService;
