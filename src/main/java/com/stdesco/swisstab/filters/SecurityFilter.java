15c8c655dee7401ec5259aeaf523d07df4e14dbe
Zach Thomas <zach.thomas@stdesco.com>
2019-02-17 00:22:05 +1000

    Final commit of the night.

--- src/main/java/com/stdesco/swisstab/filters/SecurityFilter.java
+++ src/main/java/com/stdesco/swisstab/filters/SecurityFilter.java
@@ -1,5 +1,34 @@
 package com.stdesco.swisstab.filters;
 
-public class SecurityFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SecurityFilter implements Filter {
+
+	public SecurityFilter() {
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// This is required
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, 
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() {
+		// This is required	
+	}
 
 }
