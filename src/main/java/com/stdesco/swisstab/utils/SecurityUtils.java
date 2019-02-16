9c5b21f360f52ded71fb94138bb5ed2ce9c120b4
Zach Thomas <zach.thomas@stdesco.com>
2019-02-16 23:57:29 +1000

    Completed SecurityUtils

--- src/main/java/com/stdesco/swisstab/utils/SecurityUtils.java
+++ src/main/java/com/stdesco/swisstab/utils/SecurityUtils.java
@@ -1,5 +1,63 @@
 package com.stdesco.swisstab.utils;
 
-public class SecurityUtils {
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.stdesco.swisstab.config.SecurityConfig;
+
+public class SecurityUtils {
+	@SuppressWarnings("unused")
+	private final static Logger LOGGER = 
+			Logger.getLogger(SecurityUtils.class.getName());
+	
+	/**
+	 * Check whether this request is required to login or not
+	 * 
+	 * @param request	The HttpServletRequest
+	 * 
+	 * @return			true if urlPattern has been configured for being role 
+	 * 					restricted.
+	 */
+	public static boolean isSecurityPage(HttpServletRequest request) {
+		String urlPattern = WebAppUtils.getUrlPattern(request);
+		
+		Set<String> roles = SecurityConfig.getAllAppRoles();
+		
+		for (String role : roles) {
+			List<String> urlPatterns = 
+					SecurityConfig.getUrlPatternsForRole(role);
+			if (urlPatterns != null && urlPatterns.contains(urlPattern)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Check if the role requesting this page has permission to view it.
+	 * 
+	 * @param request	The HttpServletRequest
+	 * @return			true if the requested urlPattern is configured in the 
+	 * 					requesters role's listed urlPatterns.
+	 */
+	public static boolean hasPermission(HttpServletRequest request) {
+		String urlPattern = WebAppUtils.getUrlPattern(request);
+		
+		Set<String> roles = SecurityConfig.getAllAppRoles();
+		
+		for (String role : roles) {
+			if (!request.isUserInRole(role)) {
+				continue;
+			}
+			List<String> urlPatterns = 
+					SecurityConfig.getUrlPatternsForRole(role);
+			if (urlPatterns != null && urlPatterns.contains(urlPattern)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- src/main/java/com/stdesco/swisstab/utils/WebAppUtils.java
+++ src/main/java/com/stdesco/swisstab/utils/WebAppUtils.java
@@ -1,5 +1,82 @@
 package com.stdesco.swisstab.utils;
 
+import java.util.Collection;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+import javax.servlet.http.HttpServletRequest;
+
 public class WebAppUtils {
 
+	/**
+	 * Gets the URL Pattern for the current Servlet Request.
+	 * @param request	the HttpServletRequest
+	 * @return			the URL Pattern
+	 */
+	public static String getUrlPattern(HttpServletRequest request) {
+		/* For referring to what each of these variables means in an actual web
+		 * address:
+		 * 		https://requestURL
+		 * 		https://localhost:8080/requestURI
+		 * 		https://localhost:8080/contextPath/urlPattern
+		 * 		https://localhost:8080/contextPath/servletPath/pathInfo
+		 * 
+		 * queryString adds to the end of a requestURL like:
+		 * 		https://requestURL?queryString
+		 * note: the "?" is not a part of the queryString
+		 */
+		ServletContext servletContext = request.getServletContext();
+		String urlPattern;
+		String servletPath = request.getServletPath();
+		String pathInfo = request.getPathInfo();
+		
+		if (pathInfo != null) {
+			urlPattern = servletPath + "/*";
+			return urlPattern;
+		}
+		urlPattern = servletPath;
+		
+		boolean has = hasUrlPattern(servletContext, urlPattern);
+		if (has) {
+			return urlPattern;
+		}
+		
+		int i = servletPath.lastIndexOf('.');
+		if (i != 1) {
+			String ext = servletPath.substring(i + 1);
+			urlPattern = "*." + ext;
+			has = hasUrlPattern(servletContext, urlPattern);
+			if (has) {
+				return urlPattern;
+			}
+		}
+		
+		return "/";
+	}
+
+	/**
+	 * Determines if the supplied servletContext has a particular URL Pattern.
+	 * 
+	 * @param servletContext	the servletContext you want to test for
+	 * @param urlPattern		the urlPattern you want to test for
+	 * 
+	 * @return					true if the servletRegistration mappings from
+	 * 							servletContext contains the supplied urlPattern.
+	 */
+	private static boolean hasUrlPattern(ServletContext servletContext, 
+										 String urlPattern) {
+		Map<String, ? extends ServletRegistration> map = 
+				servletContext.getServletRegistrations();
+		
+		for (String servletName : map.keySet()) {
+			ServletRegistration servletRegistration = map.get(servletName);
+			
+			Collection<String> mappings = servletRegistration.getMappings();
+			if (mappings.contains(urlPattern)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
