8c6cb0cdec1ee894fd87e91ecb00bbbe8930c923
Zach Thomas <zach.thomas@stdesco.com>
2019-02-17 17:39:10 +1000

    Minor quality of life changes

--- src/main/java/com/stdesco/swisstab/servlets/GetPropertyList.java
+++ src/main/java/com/stdesco/swisstab/servlets/GetPropertyList.java
@@ -1,5 +1,10 @@
 package com.stdesco.swisstab.servlets;
 
+/**
+ * TODO tell me what this class does. The name is far from intuitive.
+ * 
+ */
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,12 +25,9 @@ import com.google.appengine.api.datastore.Query;
 import com.stdesco.swisstab.utils.*;
 
 
-
-
 @WebServlet("/getPropertyList")
 public class GetPropertyList extends HttpServlet {
   private static final long serialVersionUID = 1l;
-  @SuppressWarnings("unused")
   private static Logger LOGGER = 
   					Logger.getLogger(GetPropertyList.class.getName());
   DatastoreService datastore = 
@@ -35,7 +37,7 @@ public class GetPropertyList extends HttpServlet {
 public void doPost(HttpServletRequest req, HttpServletResponse resp)
 		  throws ServletException, IOException {  
 	  
-	  System.out.print("GetPropertyList:41: Running \n");
+	  LOGGER.info("GetPropertyList:41: Running");
 	  
 	  //---- initialize the hash-map for response back to web-app ----//
 	  Map<String, Object> map = new HashMap<String, Object>();
@@ -46,16 +48,13 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	  
 	  //---- Run a query on kind and create a list of entities ----//
 	  Query q = new Query(kind);	
-	  PreparedQuery pq = datastore.prepare(q);		
-	  System.out.print("GetPropertyList:51:" + pq.toString());   
+	  PreparedQuery pq = datastore.prepare(q);		 
 	  List<Entity> qResult = pq.
   		  asList(FetchOptions.Builder.withDefaults());
 	
 	  List<String> propertyList = new ArrayList<String>();
 	  propertyList = DatastoreUtils.
 						 getPropertyListofEntityColumn(qResult, property);   
-	  System.out.print("GetPropertyList:66:" 
-			  							+ propertyList.toString() + "\n");
 	  
 	  map.put("propertylist", propertyList);
 	  ServletUtils.writeback(resp, map); 
